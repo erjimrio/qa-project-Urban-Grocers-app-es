c918dc1de62b53b3778535f6e1a2e5009b700196
Erick Jiménez del Río <erickjimenezdelrio@gmail.com>
2025-10-21 14:56:28 -0600

    Último commit para entrega de proyecto

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-501404c9-5d6e-4506-b334-a775451a0212.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-3056e977-2cd6-423f-b90d-d8fbec1e4874.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"  # Almacena la ruta para crear un kit en esta variable
